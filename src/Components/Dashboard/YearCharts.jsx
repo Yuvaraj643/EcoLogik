fcbe79b06744fe9578ea79a59972deefd3e9a7b5
Yuvaraj643 <vyuvarajnani2001@gmail.com>
2023-07-23 16:22:57 +0530

    now

--- src/Components/Dashboard/YearCharts.jsx
+++ src/Components/Dashboard/YearCharts.jsx
@@ -52,25 +52,25 @@ const YearCharts = () => {
 
   return (
     <>
-     <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 w-full">
-        <div class="col">
-          <div class="card">
-            <div class="card-body">
-            <PieChart />
+     <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 w-full mx-auto">
+        <div class="col ">
+          <div class="card animate__animated animate__backInLeft">
+            <div class="card-body ">
+              <PieChart />
             </div>
           </div>
         </div>
         <div class="col">
-          <div class="card">
+          <div class="card animate__animated animate__backInUp">
             <div class="card-body">
-            <ColumnChart />
+              <ColumnChart />
             </div>
           </div>
         </div>
         <div class="col">
-          <div class="card">
+          <div class="card animate__animated animate__backInRight">
             <div class="card-body">
-            <LineChart />
+              <LineChart />
             </div>
           </div>
         </div>
