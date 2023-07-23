4089a3e86b749da88121f6ed4741dde4a296a317
Yuvaraj643 <vyuvarajnani2001@gmail.com>
2023-07-23 15:48:38 +0530

    now

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -12,14 +12,14 @@ const Dashboard = () => {
   };
   return (
     <>
-    <div className =" w-full h-full">
-      <div className="text-container relative top-10 w-full  p-3 flex items-center justify-center">
-        <span>ENERGY</span>&nbsp;
-        <span>MANAGEMENT</span>
-      </div>
-      <div className="my-5 w-full ">
-        <div className="flex gap-3 justify-center">
-          <button
+      <div className=" w-full h-full">
+        <div className="text-container relative top-10 w-full  p-3 flex items-center justify-center">
+          <span>ENERGY</span>&nbsp;
+          <span>MANAGEMENT</span>
+        </div>
+        <div className="my-5 w-full ">
+          <div className="flex gap-3 justify-center">
+            {/* <button
             class="btn button-1"
             type="button"
             onClick={() => handleOptionChange("day")}
@@ -63,15 +63,48 @@ const Dashboard = () => {
               <div class="circle"></div>
               <div class="circle"></div>
             </div>
-          </button>
-        </div>
+          </button> */}
+            <button
+              data-text="Awesome"
+              class="button-1"
+              type="button"
+              onClick={() => handleOptionChange("day")}
+            >
+              <span class="actual-text">&nbsp;today&nbsp;</span>
+              <span class="hover-text" aria-hidden="true">
+                &nbsp;today&nbsp;
+              </span>
+            </button>
+            <button
+              data-text="Awesome"
+              class="button-1"
+              type="button"
+              onClick={() => handleOptionChange("month")}
+            >
+              <span class="actual-text">&nbsp;month&nbsp;</span>
+              <span class="hover-text" aria-hidden="true">
+                &nbsp;month&nbsp;
+              </span>
+            </button>
+            <button
+              data-text="Awesome"
+              class="button-1"
+              type="button"
+              onClick={() => handleOptionChange("year")}
+            >
+              <span class="actual-text">&nbsp;year&nbsp;</span>
+              <span class="hover-text" aria-hidden="true">
+                &nbsp;year&nbsp;
+              </span>
+            </button>
+          </div>
 
-        <section className="top-48 my-5 w-full">
-          {selectedOption === "day" && <DayCharts />}
-          {selectedOption === "month" && <MonthCharts />}
-          {selectedOption === "year" && <YearCharts />}
-        </section>
-      </div>
+          <section className="top-48 my-5 w-full">
+            {selectedOption === "day" && <DayCharts />}
+            {selectedOption === "month" && <MonthCharts />}
+            {selectedOption === "year" && <YearCharts />}
+          </section>
+        </div>
       </div>
     </>
   );
