1821b8e2672dce430adc4f3eecd43b623feda867
Yuvaraj643 <vyuvarajnani2001@gmail.com>
2023-07-27 18:21:55 +0530

    now

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -223,7 +223,7 @@ const Homepage = () => {
                   </span>
                 </p>
                 <a
-                  href="/Cost-dashboard"
+                  href="/C02-dashboard"
                   class="mt-10 block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Get started
