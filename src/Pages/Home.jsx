9503dee3128bc20c1ba5cd012b3d9c3a1ef28a85
Yuvaraj643 <vyuvarajnani2001@gmail.com>
2023-07-31 16:20:22 +0530

    now

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../Components/Navbar/Navbar";
-// import Dashboard from '../Components/Dashboard/Dashboard'
 import Homepage from "../Components/Homepage/Homepage";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Home = () => {
+  useEffect(() => {
+    toast.success("Click Get-Started-Buttons to view sample Dashboards---------------Team Node Noir😊", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  }, []);
   return (
     <>
+      <ToastContainer />
       <Navbar />
       <Homepage />
     </>
